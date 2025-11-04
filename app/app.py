ce87e28710c0ea4f5724500b5a60b4fa9f921dea
Evins <MAEvins@cps.k12.il.us>
2025-11-04 16:03:10 -0600

    working on posts

--- app/app.py
+++ app/app.py
@@ -2,3 +2,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# @app.get("/hello-world")
+# def hello_world():
+#     return {"message": "hello world"} #json
+
+text_posts={
+  "1":{"title":"new post", "content":"cool test post"}
+}
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+
+
+
+
+
